a58c44bea661cefa16a4d85c1d140a9292498eea
khalid mahmud <skhalidmahmud1@gmail.com>
2022-07-05 04:13:25 +0600

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,16 +1,3 @@
-<?php
-session_start();
-if (!isset($_SESSION['id'])) {
-    header('location: ../');
-}
-$data = $_SESSION['data'];
-if ($_SESSION['status'] == 1) {
-    $status = '<b class="text-successs">Voted</br>';
-} else {
-    $status = '<b class="text-danger">Not Voted</br>';
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -32,55 +19,27 @@ if ($_SESSION['status'] == 1) {
         <h1 class="my-3">Online Voting System</h1>
         <div class="row my-5">
             <div class="col md-7">
-                <?php
-                if (isset($_SESSION['groups'])) {
-                    $groups = $_SESSION['groups'];
-                    for ($i = 0; $i < count($groups); $i++) {
-                ?>
                         <div class="row">
                             <div class="col md-4">
-                                <img src="../uploads/<?php echo $groups[$i]['photo'] ?>" alt="Group Image">
+                                <img src="../uploads/GroupImg">
                             </div>
                             <div class="col md-8">
                                 <strong class="text-dark h5">Group name: </strong>
-                                <?php echo $groups[$i]['username'] ?><br>
+                                <br>
                                 <strong class="text-dark h5">Votes: </strong>
-                                <?php echo $groups[$i]['votes'] ?><br>
+                                <br>
                             </div>
                         </div>
 
                         <form action="../actions/voting.php" method="post">
-                            <input type="hidden" name="groupvotes" value="<?php echo $groups[$i]['votes']; ?>">
-                            <input type="hidden" name="groupid" value="<?php echo $groups[$i]['id']; ?>">
-                            <?php
-
-                            if ($_SESSION['status'] == 1) {
-                            ?>
-                                <button class="bg-success my-3 text-white px-3">voted</button>
-                            <?php
-
-                            } else {
-                            ?>
-                                <button class="bg-danger my-3 text-white px-3" type="submit">Vote</button>
-                            <?php
-                            }
-                            ?>
-
+                            <input type="hidden" name="groupvotes">
+                            <input type="hidden" name="groupid">
+                                <button class="bg-success my-3 text-white px-3">Vote</button>
                         </form>
                         <hr>
-                    <?php
-
-                    }
-                } else {
-                    ?>
                     <div class="container">
                         <p>No Groups to Display</p>
                     </div>
-                <?php
-
-                }
-
-                ?>
 
                 <!-- Groups -->
 
@@ -88,15 +47,15 @@ if ($_SESSION['status'] == 1) {
 
             <div class="col md-5">
                 <!-- user profile -->
-                <img src="../uploads/<?php echo $data['photo']; ?>" alt="User Image">
+                <img src="../uploads/UserImg">
                 <br>
                 <br>
                 <strong class="text-dark">Name: </strong>
-                <?php echo $data['username']; ?><br><br>
+                <br><br>
                 <strong class="text-dark">NID: </strong>
-                <?php echo $data['nid']; ?><br><br>
+                <br><br>
                 <strong class="text-dark">Status: </strong>
-                <?php echo $status; ?><br><br>
+                <br><br>
             </div>
         </div>
 
