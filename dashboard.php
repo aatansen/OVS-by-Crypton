4ffa2cf2aa4fa4f1f01424915477a3fef6dfaf4a
khalid mahmud <skhalidmahmud1@gmail.com>
2022-07-05 00:15:17 +0600

    Create dashboard.php
    Design Voting Page

---
+++ dashboard.php
@@ -0,0 +1,106 @@
+<?php
+session_start();
+if (!isset($_SESSION['id'])) {
+    header('location: ../');
+}
+$data = $_SESSION['data'];
+if ($_SESSION['status'] == 1) {
+    $status = '<b class="text-successs">Voted</br>';
+} else {
+    $status = '<b class="text-danger">Not Voted</br>';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Voting System - Dashboard</title>
+    <!-- Bootstrap CSS link  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Custom CSS link  -->
+    <link rel="stylesheet" href="../style.css">
+</head>
+
+<body class="bg-secondary text-light">
+    <div class="container my-5">
+        <a href="../"><button class="btn btn-dark text-light px-3">Back</button></a>
+        <a href="../partials/logout.php"><button class="btn btn-dark text-light px-3">Logout</button></a>
+        <h1 class="my-3">Online Voting System</h1>
+        <div class="row my-5">
+            <div class="col md-7">
+                <?php
+                if (isset($_SESSION['groups'])) {
+                    $groups = $_SESSION['groups'];
+                    for ($i = 0; $i < count($groups); $i++) {
+                ?>
+                        <div class="row">
+                            <div class="col md-4">
+                                <img src="../uploads/<?php echo $groups[$i]['photo'] ?>" alt="Group Image">
+                            </div>
+                            <div class="col md-8">
+                                <strong class="text-dark h5">Group name: </strong>
+                                <?php echo $groups[$i]['username'] ?><br>
+                                <strong class="text-dark h5">Votes: </strong>
+                                <?php echo $groups[$i]['votes'] ?><br>
+                            </div>
+                        </div>
+
+                        <form action="../actions/voting.php" method="post">
+                            <input type="hidden" name="groupvotes" value="<?php echo $groups[$i]['votes']; ?>">
+                            <input type="hidden" name="groupid" value="<?php echo $groups[$i]['id']; ?>">
+                            <?php
+
+                            if ($_SESSION['status'] == 1) {
+                            ?>
+                                <button class="bg-success my-3 text-white px-3">voted</button>
+                            <?php
+
+                            } else {
+                            ?>
+                                <button class="bg-danger my-3 text-white px-3" type="submit">Vote</button>
+                            <?php
+                            }
+                            ?>
+
+                        </form>
+                        <hr>
+                    <?php
+
+                    }
+                } else {
+                    ?>
+                    <div class="container">
+                        <p>No Groups to Display</p>
+                    </div>
+                <?php
+
+                }
+
+                ?>
+
+                <!-- Groups -->
+
+            </div>
+
+            <div class="col md-5">
+                <!-- user profile -->
+                <img src="../uploads/<?php echo $data['photo']; ?>" alt="User Image">
+                <br>
+                <br>
+                <strong class="text-dark">Name: </strong>
+                <?php echo $data['username']; ?><br><br>
+                <strong class="text-dark">NID: </strong>
+                <?php echo $data['nid']; ?><br><br>
+                <strong class="text-dark">Status: </strong>
+                <?php echo $status; ?><br><br>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
