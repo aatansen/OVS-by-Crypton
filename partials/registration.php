e0d269d7c9756aadc0c52044dea4acbe4b5e7fc4
Tansen <aatansen@gmail.com>
2022-07-12 23:37:54 +0600

    Fixed Registration page
    Format everything
    Fixed title
    NID field fixed
    password length added
    Roles field fixed
    Form action field fixed

--- partials/registration.php
+++ partials/registration.php
@@ -5,52 +5,43 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Voting System-Registration page</title>
+    <title>Online Voting System - Registration</title>
     <!-- Bootstrap CSS link  -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
-<body>
-    <h1 class="text-center text info p-3">Voting System </h1>
+<body class="bg-dark">
+    <h1 class="text-light text-center p-3">Online Voting System</h1>
     <div class="bg-info py-4">
-        <h2 class="text-center">Register</h2>
+        <h2 class="text-center">Register Account</h2>
         <div class="container text-center">
-            <form action="form.php">
+            <form action="#" method="POST" enctype="multipart/form-data">
                 <div class="mb-3">
-                    <input type="text" class="form control w-50 m-auto" placeholder="Enter your username"
-                        required="required" name="username">
-
+                    <input class="form-control w-50 m-auto" type="text" name="username" placeholder="Enter your username" required="required">
                 </div>
                 <div class="mb-3">
-                    <input type="text" class="form control w-50 m-auto" placeholder="Enter your mobile number"
-                        required="required" name="mobile">
-
+                    <input class="form-control w-50 m-auto" type="text" name="nid" placeholder="Enter your 10 digit NID number" required="required" minlength="10" maxlength="10">
                 </div>
                 <div class="mb-3">
-                    <input type="text" class="form control w-50 m-auto" placeholder="Enter your password"
-                        required="required" name="password">
-
+                    <input class="form-control w-50 m-auto" type="text" name="password" placeholder="Enter your password" required="required" minlength="7">
                 </div>
                 <div class="mb-3">
-                    <input type="text" class="form control w-50 m-auto" placeholder="Confirm password"
-                        required="required" name="cpassword">
-
+                    <input class="form-control w-50 m-auto" type="text" name="cpassword" placeholder="Confirm password" required="required" minlength="7">
                 </div>
                 <div class="mb-3">
-                    <input type="file" class="form control w-50 m-auto" name="photo">
-
+                    <input class="form-control w-50 m-auto" type="file" name="photo" data-buttonText="Your label here." required="required">
                 </div>
                 <div class="mb-3">
-                    <select name="std" class="form-select w-50 m-auto">
-                        <option value="group">Group</option>
+                    <select name="roles" class="form-select w-50 m-auto" required="required">
+                        <option value="" disabled="disabled" selected>Click here to select your role</option>
+                        <option value="candidate">Candidate</option>
                         <option value="voter">Voter</option>
                     </select>
                 </div>
-                <button type="submit" class="btn btn-datk my-4">Register</button>
-                <p>Already have an Account?
-                    <a href="../" class="text-white"></a>Logine Here
-                </p>
+                <div>
+                    <button type="submit" class="btn btn-dark my-4">Register</button>
+                </div>
+                <p>Already have an account? <a href="../" class="text-white">Login here</a></p>
             </form>
         </div>
     </div>
